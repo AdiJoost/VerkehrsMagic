9401eb6570c206320f89c1ebf086d42b5a45fd2e
AdiJoost <adi.joost@gmail.com>
2022-12-28 15:00:58 +0100

    added plotting

--- solution1/street.py
+++ solution1/street.py
@@ -1,5 +1,6 @@
 import numpy as np
 from random import randint
+from pseudocode import showMagic
 
 class Street():
 
@@ -13,13 +14,14 @@ class Street():
         self.maxSpeed = maxSpeed
         self.respawn()
 
-    def run(self, itterations):
+    def run(self, itterations, plotName):
         trafficLeft = np.zeros((itterations, self.gridsize), dtype=np.int32)
         trafficRight = np.zeros((itterations, self.gridsize), dtype=np.int32)
         for i in range(itterations):
             trafficLeft[i,:] = self.oldGridLeft[:]
             trafficRight[i,:] = self.oldGridRight[:]
             self.update()
+        showMagic(trafficLeft, trafficRight)
         
 
     def calcEmptyFieldsRight(self, pos):
